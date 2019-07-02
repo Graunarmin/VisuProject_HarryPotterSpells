7aa13f623dd2e01387b171e5ea9aa5d117767425
Johanna Sacher <johanna.sacher@uni-weimar.de>
2019-07-02 22:15:38 +0200

    started searching books for spells

--- Spells/Skripte/search_books.py
+++ Spells/Skripte/search_books.py
@@ -6,7 +6,7 @@ import pickle
 import os
 import sys
 
-def  get_spells_from_book(book, spell_list):
+def  spells_in_this_book(book, spell_list):
     """filter spell-list so only those spells that are in the books remain"""
 
     spells = []
@@ -21,38 +21,50 @@ def  get_spells_from_book(book, spell_list):
     
     return spells
 
-def load_books_and_spells():
-    """load the spells to be filtered and loop to go through all books to filter them"""
+def load_spells_from_books():
+    """load the spells to be filtered and loop to go through all books to filter them
+    No need to convert them to lower case btw, they always start with an upper case letter
+    in the books"""
     
     spells = []
     books = ['HP_01.txt', 'HP_02.txt', 'HP_03.txt', 'HP_04.txt', 'HP_05.txt', 'HP_06.txt', 'HP_07.txt']
 
     with open("../Data/spells.json", "r") as jsonfile:
-        s = json.load(jsonfile)
+        spell_json = json.load(jsonfile)
 
-
-    s = s['spell']
     spell_list = []
-    for word in s:
-        if " " in word:
-            word = word.split()
-            word = word[0]
-        spell_list.append(word)
-    #print(len(spell_list))
-    print(spell_list)
+    for s in spell_json:
+        spell_list.append(s['Short'])
     
+    print(len(spell_list))
+    print(spell_list)
 
     for book in books:
-        spells.append(get_spells_from_book(book, spell_list))
+        book_spells = spells_in_this_book(book, spell_list)
+        for spell in book_spells:
+            if spell not in spells:
+                spells.append(spell)
 
+    print(len(spells))
     print(spells)
 
+    filter_spell_json(spells, spell_json)
+
+def filter_spell_json(spell_list, spell_json):
+
+    new_json = []
+    for s in spell_json:
+        if s['Short'] in spell_list:
+            new_json.append(s)
+
+    print(len(new_json))
+    print(new_json)        
 
 def main():
     
     #file = sys.argv[1]
 
-    load_books_and_spells()
+    load_spells_from_books()
 
 if __name__ == '__main__':
     main()
