02c61d0d960501a9e61b13372833d4f7f96e4573
Johanna Sacher <johanna.sacher@uni-weimar.de>
2019-08-10 18:32:55 +0200

    changed type to UnforgivableCurse, added type to the links. Rest will come later

--- Spells/Skripte/arcdata.py
+++ Spells/Skripte/arcdata.py
@@ -7,6 +7,7 @@ def create_arcdata():
     nodes = []
     linkliste = {}
     links = []
+    types = {}
     years = ['HP_01', 'HP_02', 'HP_03', 'HP_04', 'HP_05', 'HP_06', 'HP_07']
 
     for year in years:
@@ -15,22 +16,22 @@ def create_arcdata():
             if str(element[year]):
                 #add all nodes (each spell per each year)
                 nodes.append({"spell":element["Spell"],"name":element["Short"]+"_"+year, "size":element[year],
-                            "type":element["Type"], "category":element["Category"], "danger":element["Danger"],
+                            "type":element["Type"].replace(" ",""), "category":element["Category"], "danger":element["Danger"],
                             "id":element["Short"]+"_"+year})
                 
                 #add all necessary links:
                 if element["Short"] not in linkliste:
                     linkliste[element["Short"]] = []
+                    types[element["Short"]] = element["Type"].replace(" ","")
 
                 linkliste[element["Short"]].append(element["Short"]+"_"+year) 
-
+    
     #create the links
     for spell in linkliste:
         link = linkliste[spell]
         for i in range(len(link)-1):
             for j in range(i+1,len(link)):
-                links.append({"source":link[i],"target":link[j],"value":1})
-
+                links.append({"source":link[i],"target":link[j],"type":types[spell]})
 
     data = {"nodes":nodes,"links":links}
 
